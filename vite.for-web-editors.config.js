9b1d861f7149dacb861fdae442f00a8f3394eca6
nbogie <neillbogie@googlemail.com>
2025-11-28 01:17:47 +0000

    fix misnamed "...no-bundling..." to "...for-web-editors..."

--- vite.for-web-editors.config.js
+++ vite.for-web-editors.config.js
@@ -1,4 +1,6 @@
-//WIP - not correct yet, regarding index.no-bundling.html
+//@ts-check
+
+//WIP - not correct yet, regarding index.for-web-editors.html
 
 //reading:
 //configuring vite: https://vite.dev/config/
@@ -6,25 +8,26 @@
 //building for production: https://vite.dev/guide/build
 import { defineConfig } from "vite";
 import { resolve } from "path"; // Need this to resolve the file path
-export default defineConfig(({ mode }) => {
-    // Vite automatically sets mode to 'development' for dev and 'production' for build.
-    const isProduction = mode === "production";
-
+export default defineConfig((_opts) => {
     return {
         build: {
             //more readable code for upload on openprocessing etc
             minify: false,
+            //Turn off the inclusion of the module preload polyfill at the top of the code.
+            //Removal of this polyfill could result in potentially slower module loading times
+            // for multi-file setups.
+            // But it's much nicer for the user not to see this.
+            //See https://vite.dev/config/build-options#build-modulepreload
+            modulePreload: { polyfill: false },
 
+            //https://rollupjs.org/configuration-options/
             rollupOptions: {
                 // input: {
-                //     app: isProduction
-                //         ? resolve(__dirname, "index.no-bundling.html")
-                //         : resolve(__dirname, "index.html"),
+                //     app: resolve(__dirname, "index.for-web-editors.html"),
                 // },
 
                 //externalize deps that shouldn't be bundled - e.g. p5
                 external: ["p5"],
-                //https://rollupjs.org/configuration-options/
                 preserveEntrySignatures: "strict",
                 output: {
                     format: "es",
